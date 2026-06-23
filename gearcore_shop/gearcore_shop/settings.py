db05ae4354e0b9fc46b743c47d691f96bf3cf950
Ivan <ivakhnenkoid@gmail.com>
2026-06-23 13:33:35 +0300

    update

--- gearcore_shop/gearcore_shop/settings.py
+++ gearcore_shop/gearcore_shop/settings.py
@@ -157,8 +157,9 @@ ACCOUNT_SIGNUP_PASSWORD_ENTER_TWICE = True
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp-relay.brevo.com')
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_TIMEOUT = 10
